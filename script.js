7f6f3225d644a6e56f2a9092f81d8cfec4631464
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-10 15:53:19 +0200

    Did Task2

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ const sumTo = (n) => {
 console.log(sumTo(1));
 console.log(sumTo(2));
 console.log(sumTo(3));
+
+//Task2
+function findShort(str) {
+  return str
+    .split(" ")
+    .reduce((word, current) => (word.length > current.length ? current : word));
+}
+
+const sentance = "Lorem ipsum dolor sit amet";
+
+console.log(findShort(sentance));
+console.log(findShort("Hello world"));
+console.log(findShort("Hi"));
+console.log(findShort("She is David's sister"));
