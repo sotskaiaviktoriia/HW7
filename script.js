00bace05fffd8b5acb74453c64ce291bbe1c8b4a
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-11 13:14:18 +0200

    Fixed Task1

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 "use strict";
 
 //Task1
-const sumTo = (n) => {
-  if (n === 1) {
-    return n;
-  } else {
-    return n + sumTo(n - 1);
-  }
-};
+const sumTo = (n) => (n === 1 ? n : n + sumTo(n - 1));
 
 console.log(sumTo(1));
 console.log(sumTo(2));
